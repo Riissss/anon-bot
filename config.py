58d7258cbdf7006d18193f6bfc47175101fbdd5a
Riissss <121730144+Riissss@users.noreply.github.com>
2023-01-01 01:49:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN  =  os . lingkungan . get ( "5809941857:AAFxpcoGTkXnQb5b7xjvPXslg8hIP-LbOHI" , "" ) # Dapatkan di @botfather
+TOKEN  =  os . lingkungan . get ( "5809941857:AAFxpcoGTkXnQb5b7xjvPXslg8hIP-LbOHI" ) # Dapatkan di @botfather
 OWNER = os.environ.get("OWNER", "kang_culiknew") # jangan dirubah agar tidak error
 GROUP = os.environ.get("GROUP", "") # 
 CHANNEL = os.environ.get("CHANNEL", "vvslh_pro") # 
